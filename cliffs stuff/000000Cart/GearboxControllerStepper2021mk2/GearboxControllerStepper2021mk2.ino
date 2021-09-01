d38d93fd8e9437e7001b77eceb5e50fc5234c03b
ABigRed <cliffslough@gmail.com>
2021-09-02 11:40:08 +1200

    updated gearbox controller

--- cliffs stuff/000000Cart/GearboxControllerStepper2021mk2/GearboxControllerStepper2021mk2.ino
+++ cliffs stuff/000000Cart/GearboxControllerStepper2021mk2/GearboxControllerStepper2021mk2.ino
@@ -1,6 +1,7 @@
+// for Arduino Mega
 
-int loosestop = 18;
-int tightstop = 19;
+int loosestop = 2;
+int tightstop = 4;
 int mode = 5;
 int PUL=9; //define Pulse pin
 int DIR=6; //define Direction pin
@@ -18,16 +19,16 @@ String eff="off";
 
 void setup(){
    Serial.begin(9600);
-   pinMode(loosestop,INPUT);   //micro switch pin 3, full loose.
-   pinMode(tightstop,INPUT);   //micro switch pin 4, full tight.
+   pinMode(loosestop,INPUT);   //micro switch pin 2, full loose.
+   pinMode(tightstop,INPUT);   //micro switch pin 3, full tight.
    pinMode(mode,INPUT);   //switch Sports mode, high sports,low economy.
 
-   pinMode(7, INPUT_PULLUP); 
-   attachInterrupt(digitalPinToInterrupt(7), rpm_Mot, RISING); //pin 7 is interrupt 4 (INT4).
-   pinMode(1, INPUT_PULLUP);   
-   attachInterrupt(digitalPinToInterrupt(1), rpm_PreClutch, RISING); //pin 1 is interrupt 3 (INT3). 
-   pinMode(0, INPUT_PULLUP);  
-   attachInterrupt(digitalPinToInterrupt(0), rpm_PostClutch, RISING); //pin 0 is interrupt 2 (INT2). 
+   pinMode(3, INPUT_PULLUP); 
+   attachInterrupt(digitalPinToInterrupt(3), rpm_Mot, RISING); //pin D2 is interrupt 5 (INT.5).
+   pinMode(18, INPUT_PULLUP);   
+   attachInterrupt(digitalPinToInterrupt(18), rpm_PreClutch, RISING); //pin 18 is interrupt 3 (INT.3). 
+   pinMode(19, INPUT_PULLUP);  
+   attachInterrupt(digitalPinToInterrupt(19), rpm_PostClutch, RISING); //pin 19 is interrupt 2 (INT.2). 
    pinMode(PUL,OUTPUT);
    pinMode(DIR,OUTPUT);
    pinMode(ENA,OUTPUT);
@@ -42,20 +43,39 @@ void loop(){
 void sports(){
    if(millis() - motRotation >= 2000)
    rpmMot = 0.0; 
+   rpmPreClutch = 0.0;
+   rpmPostClutch = 0.0;
+   Serial.print ("rpmMot");
+   Serial.print ( rpmMot );
+   Serial.print ("rpmPreClutch");
+   Serial.print ( rpmPreClutch );
+   Serial.print ("rpmPostClutch");
+   Serial.println ( rpmPostClutch );
    if (rpmMot < 1) {off();}
    if (rpmMot > 0 && rpmMot < 280) { anticlock() ;}
    if (rpmMot >= 280 && rpmMot <= 320) { off();}
    if (rpmMot > 320) { clockwise() ;}
+   
    loop();
   }
 
 void economy(){
+  
    if(millis() - motRotation >= 2000)
    rpmMot = 0.0;
+   rpmPreClutch = 0.0;
+   rpmPostClutch = 0.0;
+   Serial.print ("rpmMot");
+   Serial.print (rpmMot );
+   Serial.print ("rpmPreClutch");
+   Serial.print (rpmPreClutch );
+   Serial.print ("rpmPostClutch");
+   Serial.println (rpmPostClutch );
    if (rpmMot < 1) {off();}
    if (rpmMot > 0 && rpmMot < 320) { anticlock();}
    if (rpmMot >= 320 && rpmMot <= 360) { off() ;}
    if (rpmMot > 360) { clockwise();}
+  
    loop();
   }
 
@@ -66,7 +86,7 @@ void rpm_Mot(){
    
 void rpm_PreClutch(){
    rpmPreClutch  = (60000.0/(millis()-preRotation));
-   preRotation = millis();
+   preRotation = millis();  
   }
 
 void rpm_PostClutch(){
