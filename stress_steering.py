15d679a53abfac94530bd7a8d5cafb08e265142b
Masterbond7 <quintinslough@gmail.com>
2021-11-25 10:09:35 +1300

    Edited the stress test program for the improved servo performance

--- stress_steering.py
+++ stress_steering.py
@@ -3,17 +3,17 @@ import time
 bus = smbus.SMBus(1)
 
 num = 0
-op = 3
+op = 10
 
 errors = 0
 
 while True:
 	try:
-		bus.write_byte(0x12, num)
+		bus.write_byte(0x12, int(num*180/256))
 		num += op
 		if num == 256 - (256%op): op *= -1
 		if num == 0: op *= -1
-		time.sleep(10/1000)
+		time.sleep(40/1000)
 		print("Errors: {0}, Angle: {1}".format(errors, num))
 	except Exception as e:
 		print(e)
