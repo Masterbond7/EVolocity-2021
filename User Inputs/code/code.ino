cc9c29b6c54341269303d08235d212a25fc997a0
Masterbond7 <jamesslough4@gmail.com>
2021-11-25 12:20:48 +1300

    Made the arduino actually send data this time

--- User Inputs/code/code.ino
+++ User Inputs/code/code.ino
@@ -38,5 +38,10 @@ void sendData() {
     acc_pos = (analogRead(A0)-532)*(256.0/326.0); if (acc_pos <= 1) {acc_pos = 0;} if (acc_pos >= 256) {acc_pos = 255;}
     brk_pos = (analogRead(A1)-532)*(256.0/336.0); if (brk_pos <= 1) {brk_pos = 0;} if (brk_pos >= 256) {brk_pos = 255;}
     hbrk_stat = digitalRead(A10);
+
+    out_data[0] = acc_pos;
+    out_data[1] = brk_pos;
+    out_data[2] = hbrk_stat;
+    
     Wire.write(out_data, 3);
 }
