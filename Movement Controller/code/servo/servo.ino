b894f5e72eb3807c179864c7589ddb87d6a0c456
Masterbond7 <jamesslough4@gmail.com>
2022-01-28 18:51:25 +1300

    Made the steering better

--- Movement Controller/code/servo/servo.ino
+++ Movement Controller/code/servo/servo.ino
@@ -2,6 +2,7 @@
 #include <Wire.h>
 
 Servo servo;
+int desired_angle, current_angle, increment_angle, increment_delay, delta_angle, remainder_angle;
 
 union u_tag {
     unsigned short int Int;
@@ -9,18 +10,41 @@ union u_tag {
 } positionData;
 
 void setup() {
+    positionData.Int = 0;
+    
+    increment_angle = 5;
+    increment_delay = 30;
+    current_angle = 90;
+    
     servo.attach(3);
+    servo.write(current_angle);
+    
     Wire.begin(18);  
     Wire.onReceive(receiveData);
-    positionData.Int = 0;
+
+    Serial.begin(9600);
 }
 
 void loop(){
-    
+    if (current_angle > desired_angle) { current_angle -= increment_angle; }
+    else if (current_angle < desired_angle) {current_angle += increment_angle; }
+    servo.write(current_angle);
+    delay(increment_delay);
 }
 
 void receiveData(int num_bytes) {
     positionData.Char = Wire.read();
-    //positionData.Int *= (180.0f/256.0f);
-    servo.write(positionData.Int);
+    //positionData.Int = 180 - positionData.Int;//(180.0f/256.0f)
+
+    desired_angle = positionData.Int;
+    delta_angle = desired_angle - current_angle;
+    remainder_angle = delta_angle % increment_angle;
+    
+    if (remainder_angle) {
+        current_angle += delta_angle % increment_angle;
+        servo.write(current_angle);
+        delay(increment_delay);
+    }
+
+    Serial.println(desired_angle);
 }
