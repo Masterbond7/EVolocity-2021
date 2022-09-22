c1baa82ecb048a1394ef29b364501052b68817fa
Masterbond7 <jamesslough4@gmail.com>
2022-09-23 08:55:52 +1200

    Changed the code so it reads the steering position directly from the potentiometer

--- SteeringServoController/SteeringServoController.ino
+++ SteeringServoController/SteeringServoController.ino
@@ -1,8 +1,6 @@
 #include <Servo.h>
 
 Servo steeringServo;
-byte inputBytes[3];
-byte steeringByte;
 int desired_angle, current_angle, increment_angle, increment_delay, delta_angle, remainder_angle;
 
 void setup() {
@@ -15,24 +13,20 @@ void setup() {
 
     steeringServo.attach(5);
     steeringServo.write(current_angle);
+
+    pinMode(A7, INPUT);
 }
 
 void loop() {
-    if(Serial.available() > 0) {
-        //Serial.readBytes(inputBytes, 3); 
-        //steeringByte = inputBytes[0];
-        steeringByte = Serial.parseInt();
-        desired_angle = int(float(steeringByte) * (180.0f/256.0f));  
-    
-        delta_angle = desired_angle - current_angle;
-        remainder_angle = abs(delta_angle) % increment_angle;
-    
-        if (remainder_angle) {
-            if (delta_angle > 0) {current_angle += remainder_angle;}
-            if (delta_angle < 0) {current_angle -= remainder_angle;}
-            steeringServo.write(current_angle);
-            delay(increment_delay);
-        }
+    desired_angle = (int(float(analogRead(A7) * (180.0f/1024.0f))*-1)+180;
+    delta_angle = desired_angle - current_angle;
+    remainder_angle = abs(delta_angle) % increment_angle;
+
+    if (remainder_angle) {
+        if (delta_angle > 0) {current_angle += remainder_angle;}
+        if (delta_angle < 0) {current_angle -= remainder_angle;}
+        steeringServo.write(current_angle);
+        delay(int(increment_delay*(float(remainder_angle)/5.0f)));
     }
 
     if (current_angle > desired_angle) { current_angle -= increment_angle; }
