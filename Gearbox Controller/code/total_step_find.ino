d88bf0ac8785b0858f8c1510f5e360fadeef977f
Grapes42 <f.inggrapes@gmail.com>
2022-11-03 22:16:57 +1300

    Spacing

--- Gearbox Controller/code/total_step_find.ino
+++ Gearbox Controller/code/total_step_find.ino
@@ -7,6 +7,7 @@ Total steps between limit switches finder
 int
 highSensor = 7, lowSensor = 4, pulse = 11, dir = 10, stepDel = 10, stepCount = 0;
 
+
 void setup() {
   
   Serial.begin(115200);
@@ -33,6 +34,7 @@ void setup() {
   Serial.println("Total steps: "+String(stepCount));
 
 }
+
 
 void pulse() {
   digitalWrite(pulse, HIGH);
